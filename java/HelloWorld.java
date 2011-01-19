245dddb9cde94847b7f02eeacae122da98054cd0
Célio Fonseca <celio.la@gmail.com>
2011-01-19 18:18:42 -0200

    Adicionando arquivos de configuracao do eclipse. Adicionando arquivos java para teste.

---
+++ java/HelloWorld.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package teste;
+
+/**
+ *
+ * @author Yuri Maia
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        HelloWorldObject obj = new HelloWorldObject();
+        obj.executeMethod();
+    }
+
+}
