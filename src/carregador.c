fc501bcb134aec9cd554f419dd4060200408cd42
lucaspolonio <polonio.lucas@gmail.com>
2011-01-21 21:42:19 -0200

    adicionando arquivos do carregador de classes

---
+++ src/carregador.c
@@ -0,0 +1,15 @@
+/*
+ * carregador.c
+ *
+ *  Created on: 21/01/2011
+ *      Author: lucas
+ */
+
+#include "classloader.h"
+
+void carregaClasse(char *classe)
+{
+
+
+
+}
---
+++ src/carregador.h
@@ -0,0 +1,18 @@
+/*
+ * carregador.h
+ *
+ *  Created on: 21/01/2011
+ *      Author: lucas
+ */
+
+#ifndef CARREGADOR_H_
+#define CARREGADOR_H_
+
+void carregaClasse(char *classe);
+
+
+
+
+
+
+#endif /* CARREGADOR_H_ */
