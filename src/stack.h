6badfe581952f1b5bed3a6ea4e3c80e992f9facb
Célio Fonseca <celio.la@gmail.com>
2011-01-24 18:39:36 -0200

    Alterando o stack.h para usar o nosso types.h

--- src/stack.h
+++ src/stack.h
@@ -1,21 +1,24 @@
 #ifndef STACK_H
 #define STACK_H
 
-#include <stdint.h>
+#include "types.h"
 
 #define MAX_STACK 100
-#define u4 uint32_t
+
 u4 stack[MAX_STACK];
 u4 stack_top;
 u4 frame_base;
 u4 previous_frame;
 
+
+/*
+ * FUNÇÕES
+ */
 void newStack();
 void push(u4 value);
 u4 pop();
 void newFrame();
 void freeFrame();
-
 
 
 #endif
