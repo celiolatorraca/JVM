499dbf14c109cdb4084ddaae2aecab51c85ba7f9
Célio Fonseca <celio.la@gmail.com>
2011-01-25 20:17:50 -0200

    Instrucoes de pegar o resto de inteiro e de long.

--- src/instructions.c
+++ src/instructions.c
@@ -35,192 +35,192 @@ void  initializeInstr()
 	instr[0x0d] = funct_fconst_2;
 	instr[0x0e] = funct_dconst_0;
 	instr[0x0f] = funct_dconst_1;
-/*	instr[0x10] = funct_bipush;  */
-/*	instr[0x11] = funct_sipush;  */
-/*	instr[0x12] = funct_ldc;  */
-/*	instr[0x13] = funct_ldc_w;  */
-/*	instr[0x14] = funct_ldc2_w;  */
-/*	instr[0x15] = funct_iload;  */
-/*	instr[0x16] = funct_lload;  */
-/*	instr[0x17] = funct_fload;  */
-/*	instr[0x18] = funct_dload;  */
-/*	instr[0x19] = funct_aload;  */
-/*	instr[0x1a] = funct_iload_0;  */
-/*	instr[0x1b] = funct_iload_1;  */
-/*	instr[0x1c] = funct_iload_2;  */
-/*	instr[0x1d] = funct_iload_3;  */
-/*	instr[0x1e] = funct_lload_0;  */
-/*	instr[0x1f] = funct_lload_1;  */
-/*	instr[0x20] = funct_lload_2;  */
-/*	instr[0x21] = funct_lload_3;  */
-/*	instr[0x22] = funct_fload_0;  */
-/*	instr[0x23] = funct_fload_1;  */
-/*	instr[0x24] = funct_fload_2;  */
-/*	instr[0x25] = funct_fload_3;  */
-/*	instr[0x26] = funct_dload_0;  */
-/*	instr[0x27] = funct_dload_1;  */
-/*	instr[0x28] = funct_dload_2;  */
-/*	instr[0x29] = funct_dload_3;  */
-/*	instr[0x2a] = funct_aload_0;  */
-/*	instr[0x2b] = funct_aload_1;  */
-/*	instr[0x2c] = funct_aload_2;  */
-/*	instr[0x2d] = funct_aload_3;  */
-/*	instr[0x2e] = funct_iaload;  */
-/*	instr[0x2f] = funct_laload;  */
-/*	instr[0x30] = funct_faload;  */
-/*	instr[0x31] = funct_daload;  */
-/*	instr[0x32] = funct_aaload;  */
-/*	instr[0x33] = funct_baload;  */
-/*	instr[0x34] = funct_caload;  */
-/*	instr[0x35] = funct_saload;  */
-/*	instr[0x36] = funct_istore;  */
-/*	instr[0x37] = funct_lstore;  */
-/*	instr[0x38] = funct_fstore;  */
-/*	instr[0x39] = funct_dstore;  */
-/*	instr[0x3a] = funct_astore;  */
-/*	instr[0x3b] = funct_istore_0;  */
-/*	instr[0x3c] = funct_istore_1;  */
-/*	instr[0x3d] = funct_istore_2;  */
-/*	instr[0x3e] = funct_istore_3;  */
-/*	instr[0x3f] = funct_lstore_0;  */
-/*	instr[0x40] = funct_lstore_1;  */
-/*	instr[0x41] = funct_lstore_2;  */
-/*	instr[0x42] = funct_lstore_3;  */
-/*	instr[0x43] = funct_fstore_0;  */
-/*	instr[0x44] = funct_fstore_1;  */
-/*	instr[0x45] = funct_fstore_2;  */
-/*	instr[0x46] = funct_fstore_3;  */
-/*	instr[0x47] = funct_dstore_0;  */
-/*	instr[0x48] = funct_dstore_1;  */
-/*	instr[0x49] = funct_dstore_2;  */
-/*	instr[0x4a] = funct_dstore_3;  */
-/*	instr[0x4b] = funct_astore_0;  */
-/*	instr[0x4c] = funct_astore_1;  */
-/*	instr[0x4d] = funct_astore_2;  */
-/*	instr[0x4e] = funct_astore_3;  */
-/*	instr[0x4f] = funct_iastore;  */
-/*	instr[0x50] = funct_lastore;  */
-/*	instr[0x51] = funct_fastore;  */
-/*	instr[0x52] = funct_dastore;  */
-/*	instr[0x53] = funct_aastore;  */
-/*	instr[0x54] = funct_bastore;  */
-/*	instr[0x55] = funct_castore;  */
-/*	instr[0x56] = funct_sastore;  */
+	/*	instr[0x10] = funct_bipush;  */
+	/*	instr[0x11] = funct_sipush;  */
+	/*	instr[0x12] = funct_ldc;  */
+	/*	instr[0x13] = funct_ldc_w;  */
+	/*	instr[0x14] = funct_ldc2_w;  */
+	/*	instr[0x15] = funct_iload;  */
+	/*	instr[0x16] = funct_lload;  */
+	/*	instr[0x17] = funct_fload;  */
+	/*	instr[0x18] = funct_dload;  */
+	/*	instr[0x19] = funct_aload;  */
+	/*	instr[0x1a] = funct_iload_0;  */
+	/*	instr[0x1b] = funct_iload_1;  */
+	/*	instr[0x1c] = funct_iload_2;  */
+	/*	instr[0x1d] = funct_iload_3;  */
+	/*	instr[0x1e] = funct_lload_0;  */
+	/*	instr[0x1f] = funct_lload_1;  */
+	/*	instr[0x20] = funct_lload_2;  */
+	/*	instr[0x21] = funct_lload_3;  */
+	/*	instr[0x22] = funct_fload_0;  */
+	/*	instr[0x23] = funct_fload_1;  */
+	/*	instr[0x24] = funct_fload_2;  */
+	/*	instr[0x25] = funct_fload_3;  */
+	/*	instr[0x26] = funct_dload_0;  */
+	/*	instr[0x27] = funct_dload_1;  */
+	/*	instr[0x28] = funct_dload_2;  */
+	/*	instr[0x29] = funct_dload_3;  */
+	/*	instr[0x2a] = funct_aload_0;  */
+	/*	instr[0x2b] = funct_aload_1;  */
+	/*	instr[0x2c] = funct_aload_2;  */
+	/*	instr[0x2d] = funct_aload_3;  */
+	/*	instr[0x2e] = funct_iaload;  */
+	/*	instr[0x2f] = funct_laload;  */
+	/*	instr[0x30] = funct_faload;  */
+	/*	instr[0x31] = funct_daload;  */
+	/*	instr[0x32] = funct_aaload;  */
+	/*	instr[0x33] = funct_baload;  */
+	/*	instr[0x34] = funct_caload;  */
+	/*	instr[0x35] = funct_saload;  */
+	/*	instr[0x36] = funct_istore;  */
+	/*	instr[0x37] = funct_lstore;  */
+	/*	instr[0x38] = funct_fstore;  */
+	/*	instr[0x39] = funct_dstore;  */
+	/*	instr[0x3a] = funct_astore;  */
+	/*	instr[0x3b] = funct_istore_0;  */
+	/*	instr[0x3c] = funct_istore_1;  */
+	/*	instr[0x3d] = funct_istore_2;  */
+	/*	instr[0x3e] = funct_istore_3;  */
+	/*	instr[0x3f] = funct_lstore_0;  */
+	/*	instr[0x40] = funct_lstore_1;  */
+	/*	instr[0x41] = funct_lstore_2;  */
+	/*	instr[0x42] = funct_lstore_3;  */
+	/*	instr[0x43] = funct_fstore_0;  */
+	/*	instr[0x44] = funct_fstore_1;  */
+	/*	instr[0x45] = funct_fstore_2;  */
+	/*	instr[0x46] = funct_fstore_3;  */
+	/*	instr[0x47] = funct_dstore_0;  */
+	/*	instr[0x48] = funct_dstore_1;  */
+	/*	instr[0x49] = funct_dstore_2;  */
+	/*	instr[0x4a] = funct_dstore_3;  */
+	/*	instr[0x4b] = funct_astore_0;  */
+	/*	instr[0x4c] = funct_astore_1;  */
+	/*	instr[0x4d] = funct_astore_2;  */
+	/*	instr[0x4e] = funct_astore_3;  */
+	/*	instr[0x4f] = funct_iastore;  */
+	/*	instr[0x50] = funct_lastore;  */
+	/*	instr[0x51] = funct_fastore;  */
+	/*	instr[0x52] = funct_dastore;  */
+	/*	instr[0x53] = funct_aastore;  */
+	/*	instr[0x54] = funct_bastore;  */
+	/*	instr[0x55] = funct_castore;  */
+	/*	instr[0x56] = funct_sastore;  */
 	instr[0x57] = funct_pop;
 	instr[0x58] = funct_pop2;
 	instr[0x59] = funct_dup;
 	instr[0x5a] = funct_dup_x1;
-/*	instr[0x5b] = funct_dup_x2;  */
-/*	instr[0x5c] = funct_dup2;  */
-/*	instr[0x5d] = funct_dup2_x1;  */
-/*	instr[0x5e] = funct_dup2_x2;  */
-/*	instr[0x5f] = funct_swap;  */
-/*	instr[0x60] = funct_iadd;  */
-/*	instr[0x61] = funct_ladd;  */
-/*	instr[0x62] = funct_fadd;  */
-/*	instr[0x63] = funct_dadd;  */
-/*	instr[0x64] = funct_isub;  */
-/*	instr[0x65] = funct_lsub;  */
-/*	instr[0x66] = funct_fsub;  */
-/*	instr[0x67] = funct_dsub;  */
-/*	instr[0x68] = funct_imul;  */
-/*	instr[0x69] = funct_lmul;  */
-/*	instr[0x6a] = funct_fmul;  */
-/*	instr[0x6b] = funct_dmul;  */
-/*	instr[0x6c] = funct_idiv;  */
-/*	instr[0x6d] = funct_ldiv;  */
-/*	instr[0x6e] = funct_fdiv;  */
-/*	instr[0x6f] = funct_ddiv;  */
-/*	instr[0x70] = funct_irem;  */
-/*	instr[0x71] = funct_lrem;  */
-/*	instr[0x72] = funct_frem;  */
-/*	instr[0x73] = funct_drem;  */
-/*	instr[0x74] = funct_ineg;  */
-/*	instr[0x75] = funct_lneg;  */
-/*	instr[0x76] = funct_fneg;  */
-/*	instr[0x77] = funct_dneg;  */
-/*	instr[0x78] = funct_ishl;  */
-/*	instr[0x79] = funct_lshl;  */
-/*	instr[0x7a] = funct_ishr;  */
-/*	instr[0x7b] = funct_lshr;  */
-/*	instr[0x7c] = funct_iushr;  */
-/*	instr[0x7d] = funct_lushr;  */
-/*	instr[0x7e] = funct_iand;  */
-/*	instr[0x7f] = funct_land;  */
-/*	instr[0x80] = funct_ior;  */
-/*	instr[0x81] = funct_lor;  */
-/*	instr[0x82] = funct_ixor;  */
-/*	instr[0x83] = funct_lxor;  */
-/*	instr[0x84] = funct_iinc;  */
-/*	instr[0x85] = funct_i2l;  */
-/*	instr[0x86] = funct_i2f;  */
-/*	instr[0x87] = funct_i2d;  */
-/*	instr[0x88] = funct_l2i;  */
-/*	instr[0x89] = funct_l2f;  */
-/*	instr[0x8a] = funct_l2d;  */
-/*	instr[0x8b] = funct_f2i;  */
-/*	instr[0x8c] = funct_f2l;  */
-/*	instr[0x8d] = funct_f2d;  */
-/*	instr[0x8e] = funct_d2i;  */
-/*	instr[0x8f] = funct_d2l;  */
-/*	instr[0x90] = funct_d2f;  */
-/*	instr[0x91] = funct_i2b;  */
-/*	instr[0x92] = funct_i2c;  */
-/*	instr[0x93] = funct_i2s;  */
-/*	instr[0x94] = funct_lcmp;  */
-/*	instr[0x95] = funct_fcmpl;  */
-/*	instr[0x96] = funct_fcmpg;  */
-/*	instr[0x97] = funct_dcmpl;  */
-/*	instr[0x98] = funct_dcmpg;  */
-/*	instr[0x99] = funct_ifeq;  */
-/*	instr[0x9a] = funct_ifne;  */
-/*	instr[0x9b] = funct_iflt;  */
-/*	instr[0x9c] = funct_ifge;  */
-/*	instr[0x9d] = funct_ifgt;  */
-/*	instr[0x9e] = funct_ifle;  */
-/*	instr[0x9f] = funct_if_icmpeq;  */
-/*	instr[0xa0] = funct_if_icmpne;  */
-/*	instr[0xa1] = funct_if_icmplt;  */
-/*	instr[0xa2] = funct_if_icmpge;  */
-/*	instr[0xa3] = funct_if_icmpgt;  */
-/*	instr[0xa4] = funct_if_icmple;  */
-/*	instr[0xa5] = funct_if_acmpeq;  */
-/*	instr[0xa6] = funct_if_acmpne;  */
-/*	instr[0xa7] = funct_goto;  */
-/*	instr[0xa8] = funct_jsr;  */
-/*	instr[0xa9] = funct_ret;  */
-/*	instr[0xaa] = funct_tableswitch;  */
-/*	instr[0xab] = funct_lookupswitch;  */
-/*	instr[0xac] = funct_ireturn;  */
-/*	instr[0xad] = funct_lreturn;  */
-/*	instr[0xae] = funct_freturn;  */
-/*	instr[0xaf] = funct_dreturn;  */
-/*	instr[0xb0] = funct_areturn;  */
-/*	instr[0xb1] = funct_return;  */
-/*	instr[0xb2] = funct_getstatic;  */
-/*	instr[0xb3] = funct_putstatic;  */
-/*	instr[0xb4] = funct_getfield;  */
-/*	instr[0xb5] = funct_putfield;  */
-/*	instr[0xb6] = funct_invokevirtual;  */
-/*	instr[0xb7] = funct_invokespecial;  */
-/*	instr[0xb8] = funct_invokestatic;  */
-/*	instr[0xb9] = funct_invokeinterface;  */
-/*	instr[0xba] = funct_nao_utilizada;  */
-/*	instr[0xbb] = funct_new;  */
-/*	instr[0xbc] = funct_newarray;  */
-/*	instr[0xbd] = funct_anewarray;  */
-/*	instr[0xbe] = funct_arraylength;  */
-/*	instr[0xbf] = funct_athrow;  */
-/*	instr[0xc0] = funct_checkcast;  */
-/*	instr[0xc1] = funct_instanceof;  */
-/*	instr[0xc2] = funct_monitorenter;  */
-/*	instr[0xc3] = funct_monitorexit;  */
-/*	instr[0xc4] = funct_wide;  */
-/*	instr[0xc5] = funct_multianewarray;  */
-/*	instr[0xc6] = funct_ifnull;  */
-/*	instr[0xc7] = funct_ifnonull;  */
-/*	instr[0xc8] = funct_goto_w;  */
-/*	instr[0xc9] = funct_jsr_w;  */
+	/*	instr[0x5b] = funct_dup_x2;  */
+	/*	instr[0x5c] = funct_dup2;  */
+	/*	instr[0x5d] = funct_dup2_x1;  */
+	/*	instr[0x5e] = funct_dup2_x2;  */
+	/*	instr[0x5f] = funct_swap;  */
+	/*	instr[0x60] = funct_iadd;  */
+	/*	instr[0x61] = funct_ladd;  */
+	/*	instr[0x62] = funct_fadd;  */
+	/*	instr[0x63] = funct_dadd;  */
+	/*	instr[0x64] = funct_isub;  */
+	/*	instr[0x65] = funct_lsub;  */
+	/*	instr[0x66] = funct_fsub;  */
+	/*	instr[0x67] = funct_dsub;  */
+	/*	instr[0x68] = funct_imul;  */
+	/*	instr[0x69] = funct_lmul;  */
+	/*	instr[0x6a] = funct_fmul;  */
+	/*	instr[0x6b] = funct_dmul;  */
+	/*	instr[0x6c] = funct_idiv;  */
+	/*	instr[0x6d] = funct_ldiv;  */
+	/*	instr[0x6e] = funct_fdiv;  */
+	/*	instr[0x6f] = funct_ddiv;  */
+	instr[0x70] = funct_irem;
+	instr[0x71] = funct_lrem;
+	/*	instr[0x72] = funct_frem;  */
+	/*	instr[0x73] = funct_drem;  */
+	/*	instr[0x74] = funct_ineg;  */
+	/*	instr[0x75] = funct_lneg;  */
+	/*	instr[0x76] = funct_fneg;  */
+	/*	instr[0x77] = funct_dneg;  */
+	/*	instr[0x78] = funct_ishl;  */
+	/*	instr[0x79] = funct_lshl;  */
+	/*	instr[0x7a] = funct_ishr;  */
+	/*	instr[0x7b] = funct_lshr;  */
+	/*	instr[0x7c] = funct_iushr;  */
+	/*	instr[0x7d] = funct_lushr;  */
+	/*	instr[0x7e] = funct_iand;  */
+	/*	instr[0x7f] = funct_land;  */
+	/*	instr[0x80] = funct_ior;  */
+	/*	instr[0x81] = funct_lor;  */
+	/*	instr[0x82] = funct_ixor;  */
+	/*	instr[0x83] = funct_lxor;  */
+	/*	instr[0x84] = funct_iinc;  */
+	/*	instr[0x85] = funct_i2l;  */
+	/*	instr[0x86] = funct_i2f;  */
+	/*	instr[0x87] = funct_i2d;  */
+	/*	instr[0x88] = funct_l2i;  */
+	/*	instr[0x89] = funct_l2f;  */
+	/*	instr[0x8a] = funct_l2d;  */
+	/*	instr[0x8b] = funct_f2i;  */
+	/*	instr[0x8c] = funct_f2l;  */
+	/*	instr[0x8d] = funct_f2d;  */
+	/*	instr[0x8e] = funct_d2i;  */
+	/*	instr[0x8f] = funct_d2l;  */
+	/*	instr[0x90] = funct_d2f;  */
+	/*	instr[0x91] = funct_i2b;  */
+	/*	instr[0x92] = funct_i2c;  */
+	/*	instr[0x93] = funct_i2s;  */
+	/*	instr[0x94] = funct_lcmp;  */
+	/*	instr[0x95] = funct_fcmpl;  */
+	/*	instr[0x96] = funct_fcmpg;  */
+	/*	instr[0x97] = funct_dcmpl;  */
+	/*	instr[0x98] = funct_dcmpg;  */
+	/*	instr[0x99] = funct_ifeq;  */
+	/*	instr[0x9a] = funct_ifne;  */
+	/*	instr[0x9b] = funct_iflt;  */
+	/*	instr[0x9c] = funct_ifge;  */
+	/*	instr[0x9d] = funct_ifgt;  */
+	/*	instr[0x9e] = funct_ifle;  */
+	/*	instr[0x9f] = funct_if_icmpeq;  */
+	/*	instr[0xa0] = funct_if_icmpne;  */
+	/*	instr[0xa1] = funct_if_icmplt;  */
+	/*	instr[0xa2] = funct_if_icmpge;  */
+	/*	instr[0xa3] = funct_if_icmpgt;  */
+	/*	instr[0xa4] = funct_if_icmple;  */
+	/*	instr[0xa5] = funct_if_acmpeq;  */
+	/*	instr[0xa6] = funct_if_acmpne;  */
+	/*	instr[0xa7] = funct_goto;  */
+	/*	instr[0xa8] = funct_jsr;  */
+	/*	instr[0xa9] = funct_ret;  */
+	/*	instr[0xaa] = funct_tableswitch;  */
+	/*	instr[0xab] = funct_lookupswitch;  */
+	/*	instr[0xac] = funct_ireturn;  */
+	/*	instr[0xad] = funct_lreturn;  */
+	/*	instr[0xae] = funct_freturn;  */
+	/*	instr[0xaf] = funct_dreturn;  */
+	/*	instr[0xb0] = funct_areturn;  */
+	/*	instr[0xb1] = funct_return;  */
+	/*	instr[0xb2] = funct_getstatic;  */
+	/*	instr[0xb3] = funct_putstatic;  */
+	/*	instr[0xb4] = funct_getfield;  */
+	/*	instr[0xb5] = funct_putfield;  */
+	/*	instr[0xb6] = funct_invokevirtual;  */
+	/*	instr[0xb7] = funct_invokespecial;  */
+	/*	instr[0xb8] = funct_invokestatic;  */
+	/*	instr[0xb9] = funct_invokeinterface;  */
+	/*	instr[0xba] = funct_nao_utilizada;  */
+	/*	instr[0xbb] = funct_new;  */
+	/*	instr[0xbc] = funct_newarray;  */
+	/*	instr[0xbd] = funct_anewarray;  */
+	/*	instr[0xbe] = funct_arraylength;  */
+	/*	instr[0xbf] = funct_athrow;  */
+	/*	instr[0xc0] = funct_checkcast;  */
+	/*	instr[0xc1] = funct_instanceof;  */
+	/*	instr[0xc2] = funct_monitorenter;  */
+	/*	instr[0xc3] = funct_monitorexit;  */
+	/*	instr[0xc4] = funct_wide;  */
+	/*	instr[0xc5] = funct_multianewarray;  */
+	/*	instr[0xc6] = funct_ifnull;  */
+	/*	instr[0xc7] = funct_ifnonull;  */
+	/*	instr[0xc8] = funct_goto_w;  */
+	/*	instr[0xc9] = funct_jsr_w;  */
 }
 
 void funct_nop()
@@ -608,9 +608,52 @@ void funct_idiv(){ current_frame.pc++;  }
 void funct_ldiv(){ current_frame.pc++;  }
 void funct_fdiv(){ current_frame.pc++;  }
 void funct_ddiv(){ current_frame.pc++;  }
-void funct_irem(){ current_frame.pc++;  }
-void funct_lrem(){ current_frame.pc++;  }
-void funct_frem(){ current_frame.pc++;  }
+
+void funct_irem()
+{
+	u4 value1, value2;
+
+	value2 = pop();
+	value1 = pop();
+
+	push( value2 % value1 );
+
+	current_frame.pc++;
+
+}
+void funct_lrem()
+{
+	int64_t aux1, aux2;
+	u4 low, high, aux3;
+
+	low = pop();
+	high = pop();
+	aux2 = (signed) convert_to_64_bits( low, high );
+
+	low = pop();
+	high = pop();
+	aux1 = (signed) convert_to_64_bits( low, high );
+
+	aux1 = aux1 % aux2;
+
+	/* 32 bits mais altos */
+	aux3 = aux1 >> 32;
+	push ( aux3 );
+
+	/* 32 bits mais baixos */
+	aux3 = aux1 & 0xffffffff;
+	push( aux3 );
+
+	current_frame.pc++;
+}
+
+void funct_frem()
+{
+
+
+	current_frame.pc++;
+}
+
 void funct_drem(){ current_frame.pc++;  }
 void funct_ineg(){ current_frame.pc++;  }
 void funct_lneg(){ current_frame.pc++;  }
