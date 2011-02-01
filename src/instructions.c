eb1bb472cf5222676d066b271f46f2ac3b5bab6e
lucaspolonio <polonio.lucas@gmail.com>
2011-01-31 22:11:14 -0200

    implementada invokestatic e mudancas na prepareMethod

--- src/instructions.c
+++ src/instructions.c
@@ -3376,7 +3376,60 @@ void funct_invokespecial()
 	current_frame->pc++;
 }
 
-void funct_invokestatic(){ current_frame->pc++;current_frame->pc++;current_frame->pc++; }
+void funct_invokestatic(){
+
+	u4 index;
+	u1 low, high;
+	int32_t numParams, i;
+	int32_t class_index, class_index_tmp;
+	u2 name_type_index;
+	char *class_name;
+	u4 *fields_tmp;
+
+	struct ClassFile *class;
+	method_info *method;
+
+	high = current_frame->code[++(current_frame->pc)];
+	low = current_frame->code[++(current_frame->pc)];
+
+	index = convert_2x8_to_32_bits(low, high);
+
+
+	class_index_tmp = ((struct CONSTANT_Methodref_info *)(current_frame->constant_pool[index-1]))->class_index;
+
+	class_name = getName(current_frame->class,
+			((struct CONSTANT_Class_info *)(current_frame->constant_pool[class_index_tmp-1]))->name_index);
+
+
+	class_index = loadClass( class_name );
+	class = getClassByIndex( class_index );
+
+	name_type_index = ((struct CONSTANT_Methodref_info *)(current_frame->constant_pool[index-1]))->name_and_type_index;
+
+	method = getMethodByNameAndDescIndex(class, current_frame->class, name_type_index);
+
+#ifdef DEBUG
+	printf("invokestatic %s -> %s\n", class_name, getName(class, method->name_index));
+#endif
+
+	numParams = getNumParameters( class , method );
+
+	fields_tmp = calloc(sizeof(u4),numParams+1);
+	for (i = numParams; i > 0; i--) { /* única diferença pra invokespecial */
+		fields_tmp[i] = pop();
+	}
+
+	prepareMethod(class, method);
+
+	for (i = numParams; i >= 0; i--) {
+		current_frame->fields[i] = fields_tmp[i];
+	}
+
+	runMethod();
+
+	current_frame->pc++;
+
+}
 void funct_invokeinterface(){ current_frame->pc++;  }
 /*void funct_nao_utilizada;*/
 
