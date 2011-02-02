c2bea630c66bd706e387e4c15490722c52062d83
maiayuri <maiayuri@hotmail.com>
2011-02-02 06:51:50 -0300

    Javas

---
+++ src/javaTeste/Heranca.java
@@ -0,0 +1,13 @@
+package javaTeste;
+
+class Heranca
+{
+	public static void main(String[] args)
+	{
+		Filho f = new Filho();
+		f.methodFilho();
+		f.method();
+		f.method2();
+		f.field_pai = 1;
+	}
+}
---
+++ src/javaTeste/Pai.java
@@ -0,0 +1,20 @@
+package javaTeste;
+
+class Pai
+{
+	int field_pai;
+	boolean field_pai2;
+
+	public void method()
+	{
+		int field;
+	}
+
+	protected void method2()
+	{
+	}
+
+	private void method3()
+	{
+	}
+}
