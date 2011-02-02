ee76f25f7e2ecee4059738870ae9038ee91512a2
Célio Fonseca <celio.la@gmail.com>
2011-02-02 02:12:53 -0200

    Correcao de muitas instrucoes qndo executamos os testes do ladeira...

--- src/javaTeste/PrintTeste.java
+++ src/javaTeste/PrintTeste.java
@@ -3,15 +3,16 @@ package testeJava;
 public class PrintTeste {
 	
 	public static void main(String[] args) {
-		int a = 3;
-		System.out.println("Deu certo!"+a);
-		
+		double a = 3.0;
+		boolean b = true;
+		System.out.print(a+""+b);
+		/*
 		Object o = new Object();
 		System.out.println(o);
 		
 		a = 10;
 		System.out.print(a);
-		System.out.print((double)a/2);
+		System.out.print((double)a/2);*/
 	}
 	
 }
